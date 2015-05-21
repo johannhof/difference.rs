c4adb4b4b02b846989130f308bad76a965eb3911
Johann <git@johann-hofmann.com>
2015-05-21 07:53:41 +0200

    Docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,30 @@
+//! Functions to find the difference between to texts (strings).
+
+#![crate_name = "text_diff"]
+
 #![feature(step_by)]
 
+// I can basically feel the karma already
+#![deny(missing_docs)]
+
+/// Defines the contents of a changeset
+/// Changesets will be delivered in order of appearance in the original string
+/// Sequences of the same kind will be grouped into one Difference
 #[derive(PartialEq, Debug)]
 pub enum Difference {
+    /// Sequences that are the same
     Same(String),
+    /// Sequences that are an addition (don't appear in the first string)
     Add(String),
+    /// Sequences that are a removal (don't appear in the second string)
     Rem(String)
+}
+
+/// Calculates the edit distance and the changeset for two given strings
+/// The first string is assumed to be the "original", the second to be an
+/// edited version of the first
+pub fn diff(orig: &str, edit: &str) -> (i32, String) {
+    lcs(orig, edit)
 }
 
 fn lcs(orig: &str, edit: &str) -> (i32, String) {
